99cd59e9061df0d13c8c8f788f415f51f7ef51ca
Md. Abu Sayed <57618398+abusayedk71@users.noreply.github.com>
2021-03-09 23:04:08 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.views import LoginView , LogoutView
 
 urlpatterns = [
-    path('', views.home,name='home'),
+    path('dashboard/', views.home,name='home'),
     path('accounts/login/',LoginView.as_view(),name='login'),
     path('accounts/logout/',LogoutView.as_view(),name='logout'),
     path('register/', views.registraionPage,name='registraion'),
